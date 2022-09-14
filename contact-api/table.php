ef316b3dccfd874bebd131ccb2f8d41dbe0ddd59
Hadi Darwish <hadi.darwish.03@gmail.com>
2022-09-14 12:10:37 +0300

    adding api of data extraction

---
+++ contact-api/table.php
@@ -0,0 +1,19 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
+
+include("connection.php");
+$queryText = "SELECT * FROM contact";
+$query = $mysqli->prepare($queryText);
+$query->execute();
+$array = $query->get_result();
+
+$response = [];
+
+while ($a = $array->fetch_assoc()) {
+    $response[] = $a;
+}
+
+$json = json_encode($response);
+echo $json;
